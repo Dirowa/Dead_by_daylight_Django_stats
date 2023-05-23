8cb91d9884c5b1d2f5531dc365aeec5b386174c4
Donny Vrins <62692537+Dirowa@users.noreply.github.com>
2023-05-23 12:43:53 +0200

    browsableapi enabled

--- DBD/dbd_stats/urls.py
+++ DBD/dbd_stats/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path("submit_data", views.get_game_data, name="submit_data"),
     path("submit_data/thanks", views.thanks, name="Thanks"),
     path("stats", views.statistics, name="stats"),
-    path("api",views.game_list),
-    path("api/<int:pk>/", views.game_detail)
+    path("api",views.game_list.as_view()),
+    path("api/<int:pk>/", views.GamesDetail.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- DBD/dbd_stats/views.py
+++ DBD/dbd_stats/views.py
@@ -7,11 +7,14 @@ from django.shortcuts import render
 from .forms import Game_form_entree
 import datetime
 
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, Http404
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from .models import Game
 from .serializers import GameSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 def index(request):
     return render(request, "dbd_stats/welcome_page.html")
@@ -43,43 +46,46 @@ def thanks(request):
 def statistics(request):
     return render(request, "dbd_stats/statistics.html")
 
-@api_view(["GET","POST"])
-def game_list(request,format=None):
-    if request.method == "GET":
-        game = Game.objects.all()
-        serializer = GameSerializer(game,many=True)
-        return(JsonResponse(serializer.data,safe=False))
+class game_list(APIView):
+    """
+    List all snippets, or create a new snippet.
+    """
+    def get(self, request, format=None):
+        snippets = Game.objects.all()
+        serializer = GameSerializer(snippets, many=True)
+        return Response(serializer.data)
 
-    elif request.method == "POST":
-        data =JSONParser().parse(request)
-        serializer = GameSerializer(data=data)
+    def post(self, request, format=None):
+        serializer = GameSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data,status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def game_detail(request, pk,format=None):
+class GamesDetail(APIView):
     """
-    Retrieve, update or delete a code snippet.
+    Retrieve, update or delete a snippet instance.
     """
-    try:
-        snippet = Game.objects.get(pk=pk)
-    except Game.DoesNotExist:
-        return HttpResponse(status=404)
+    def get_object(self, pk):
+        try:
+            return Game.objects.get(pk=pk)
+        except Game.DoesNotExist:
+            raise Http404
 
-    if request.method == 'GET':
-        serializer = GameSerializer(snippet)
-        return JsonResponse(serializer.data)
+    def get(self, request, pk, format=None):
+        game = self.get_object(pk)
+        serializer = GameSerializer(game)
+        return Response(serializer.data)
 
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = GameSerializer(snippet, data=data)
+    def put(self, request, pk, format=None):
+        game = self.get_object(pk)
+        serializer = GameSerializer(game, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
-        Game.delete()
-        return HttpResponse(status=204)
+    def delete(self, request, pk, format=None):
+        game = self.get_object(pk)
+        game.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
