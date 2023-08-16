3b0c7e5b0c4f816ffef358487f2176f28b6b9bf0
Donny Vrins <62692537+Dirowa@users.noreply.github.com>
2023-08-16 10:16:23 +0200

    updates

--- DBD/dbd_stats/utils.py
+++ DBD/dbd_stats/utils.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import base64
 from io import  BytesIO
 from datetime import datetime
+from .models import killer_choices
 
 
 def get_graph():
@@ -35,7 +36,7 @@ class CreateData():
     def get_plot_histogram(self):
         plt.switch_backend("AGG")
 
-        days = list((self.how_many_games).keys())
+        days = list(self.how_many_games)
 
 
         plt.figure(figsize=(10,5))
@@ -43,3 +44,21 @@ class CreateData():
         plt.hist(self.how_many_games)
         graph = get_graph()
         return graph
+
+    def get_killer_a_day(self):
+        days = []
+        killer_count = {}
+        for i in self.data.keys():
+            ii = (i.split("T"))[0]
+            if ii not in killer_count.keys():
+                killer_count[ii] = {}
+
+
+
+
+
+        Days = data1.keys()
+
+        #Counts = {
+        #   Killer1: np.array[count date1, date2,date3]
+        #   Killer 2: np.array[cunt date1,date2,date3]
